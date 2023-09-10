e665887d78c0d860181a4e3f3245520f8d8fa701
Faisal Khan <faisalkhan@Faisals-MacBook-Pro.local>
2023-09-10 11:43:46 +0200

    Fix Mongodb database adaptor

--- chatterbot/storage/mongodb.py
+++ chatterbot/storage/mongodb.py
@@ -138,7 +138,7 @@ class MongoDatabaseAdapter(StorageAdapter):
             for order in order_by:
                 mongo_ordering.append((order, pymongo.ASCENDING))
 
-        total_statements = self.statements.find(kwargs).count()
+        total_statements = self.statements.count_documents(kwargs)
 
         for start_index in range(0, total_statements, page_size):
             if mongo_ordering:
